418e16d007f028a74670a93f2eb1a24b0ef32df5
DraperStudio <hello@draperstud.io>
2015-07-16 08:12:39 +0200

    Publish migration with fresh timestamp

--- src/MessageableServiceProvider.php
+++ src/MessageableServiceProvider.php
@@ -14,8 +14,13 @@ class MessageableServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $fromPath = __DIR__.'/../database/migrations/';
+        $toPath = database_path('/migrations/');
+
         $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('/migrations'),
+            $fromPath.'create_messages_table.php' => $toPath.date('Y_m_d_His', time()).'_create_messages_table.php',
+            $fromPath.'create_participants_table.php' => $toPath.date('Y_m_d_His', time()).'_create_participants_table.php',
+            $fromPath.'create_threads_table.php' => $toPath.date('Y_m_d_His', time()).'_create_threads_table.php',
         ], 'migrations');
     }
 
